3401c8d45021aa883a4abd9ae7af69dc90a48375
emipa606 <emil.offermo@litet.net>
2021-07-16 20:21:29 +0200

    Mod updated for 1.3 and passed autotests

---
+++ Source/Borg/Hediff_BorgInfection.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using Verse; // Base C# library
+// RimWorld's base game library. Contains lots of hardcodedgame data
+// RimWorld's second main library. Holds more "functional" classes than RimWorld's game data classes
+// Reflection is a way to access private data that is origally intended to not be accessed or simply hidden
+
+
+namespace BorgAssimilate
+{
+    public class Hediff_BorgInfection : Hediff_Injury
+    {
+        public override void Notify_PawnDied()
+        {
+            base.Notify_PawnDied();
+
+            if (pawn.def.race.Animal == false)
+            {
+                var corpse = pawn.Corpse;
+                var newBorg = PawnGenerator.GeneratePawn(PawnKindDef.Named("BorgDrone3"),
+                    FactionUtility.DefaultFactionFrom(FactionDef.Named("BorgCollective")));
+                newBorg.Position = corpse.Position;
+                newBorg.SpawnSetup(corpse.Map, false);
+
+                if (corpse != null)
+                {
+                    corpse.Destroy();
+                }
+            }
+            else if (pawn.def.race.Animal)
+            {
+                Messages.Message(
+                    "an animal has succumbed to nanite infection, and have been deemed inappropriate for assimilation. The nanites have consumed and destroyed the corpse.",
+                    MessageTypeDefOf.NeutralEvent);
+                pawn.Corpse.Destroy();
+            }
+        }
+    }
+}
---
+++ Source/Borg/Hediff_BorgInfectionPlayer.cs
@@ -0,0 +1,35 @@
+﻿using RimWorld;
+using Verse;
+
+namespace BorgAssimilate
+{
+    public class Hediff_BorgInfectionPlayer : Hediff_Injury
+    {
+        public override void Notify_PawnDied()
+        {
+            base.Notify_PawnDied();
+
+            if (pawn.def.race.Animal == false)
+            {
+                var corpse = pawn.Corpse;
+                var newBorg1 = PawnGenerator.GeneratePawn(PawnKindDef.Named("PlayerBorgDrone"),
+                    FactionUtility.DefaultFactionFrom(FactionDef.Named("BorgCollective")));
+                newBorg1.SetFaction(Faction.OfPlayer);
+                newBorg1.Position = corpse.Position;
+                newBorg1.SpawnSetup(corpse.Map, false);
+
+                if (corpse != null)
+                {
+                    corpse.Destroy();
+                }
+            }
+            else if (pawn.def.race.Animal)
+            {
+                Messages.Message(
+                    "an animal has succumbed to the nanite infection, and have been deemed inappropriate for assimilation. The nanites have consumed and destroyed the corpse.",
+                    MessageTypeDefOf.NeutralEvent);
+                pawn.Corpse.Destroy();
+            }
+        }
+    }
+}
